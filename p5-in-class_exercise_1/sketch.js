92cc12ffdb4bbfa5887e15c2bf5c722c5e4c12e4
Zach Fisher <zachary.fisher@sjsu.edu>
2018-10-11 11:33:27 -0700

    js stuff

---
+++ p5-in-class_exercise_1/sketch.js
@@ -0,0 +1,13 @@
+// declare variables at top
+
+function setup() {
+  // runs once
+  console.log("in setup function")
+}
+
+function draw() {
+  // loops over and over
+  console.log("in draw function")
+}
+
+// write functions here
